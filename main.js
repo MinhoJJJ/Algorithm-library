bba8772b54eafa2b4891201bed1b8aeaf060427b
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-03-19 12:22:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-main.js 
+//main.js 
 
 //부트스트랩
 import 'bootstrap' 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { createApp } from 'vue'
+import App from './App.vue'
 
-npm install bootstrap@5
+createApp(App).mount('#app')
+
+
+
+// 부트스트랩 설치 npm install bootstrap@5
