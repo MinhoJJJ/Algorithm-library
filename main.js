4458cce7b6e0ec0eb1088442455a659905bd19bb
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-03-21 22:49:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,5 +10,6 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 
-
-// 부트스트랩 설치 npm install bootstrap@5
+// 라우터 설치 npm install vue-router@4
+// 부트스트랩 설치 npm install bootstrap@5\
+// 서버 켜져있다면 끄고 할 것
