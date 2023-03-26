ed034dd1a2c0c3e1259b1698fa0a3c212d8c8181
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-03-26 18:53:20 +0900

    Update Vue.js

--- Vue.js
+++ Vue.js
@@ -2,3 +2,18 @@
 <style>
 @import 'style.css';
 </style>
+
+// 데이터 바인딩
+<Container/>
+</template>
+
+<script>
+
+import Container from './components/Container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
