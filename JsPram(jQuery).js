1b3dbde8b7ea06a3adfbd34b73723dfe6bb18476
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-04-29 23:30:11 +0900

    Update JsPram(jQuery).js

--- JsPram(jQuery).js
+++ JsPram(jQuery).js
@@ -11,6 +11,15 @@ $('[name="my-input"]') // name="my-input"인 모든 요소를 선택합니다.
 ${param}               // Model 객체에서 param이라는 이름으로 저장된 변수의 값을 출력합니다.
 $('#list #hello').val("Y"); //해당 코드는 id가 "list"인 요소 안에 있는 id가 "hello"인 요소의 값을 "Y"로 설정하는 코드
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> : JSTL 태그라이브러리에서 core 태그 라이브러리를
+의미하며 core 태그를 사용할때 c를 붙여서 사용하도록 하는 것입니다.
+
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %> : JSTL 태그라이브러리에서 fn 태그 라이브러리를
+의미하며 fn 태그를 사용할때 fn를 붙여서 사용하도록 하는 것입니다.
+
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>: JSTL 태그라이브러리에서 fmt 태그 라이브러리를
+의미하며 fmt 태그를 사용할때 fmt를 붙여서 사용하도록 하는 것입니다. fmt는 보통 다국어 처리, 날짜/시간 형식 지정등과 관련된 기능을 제공합니다.
+
 
 
 // 해당값을 get으로 날리고 싶다면
