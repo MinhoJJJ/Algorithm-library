081856441d2738762f108dbc3706fb6bea62a7aa
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2023-05-03 10:12:57 +0900

    Update JsPram(jQuery).js

--- JsPram(jQuery).js
+++ JsPram(jQuery).js
@@ -1,8 +1,6 @@
 // jsp에서 mtml부분의 데이터를 쓰고 싶다면 
 var minho = $('#minho').val(); 
 
-
-
 $(".hi")               // jQuery 라이브러리에서 제공하는 함수 중 하나로 html단에서 class="hi"인 것을 찾아줍니다.
 $('div')               // jQuery 라이브러리에서 제공하는 함수 중 하나로 html단 모든 div요소를 찾습니다.
 $('ul li')             // <ul> 요소 아래에 있는 모든 <li> 요소를 선택합니다.
@@ -20,6 +18,7 @@ $('#list #hello').val("Y"); //해당 코드는 id가 "list"인 요소 안에 있
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>: JSTL 태그라이브러리에서 fmt 태그 라이브러리를
 의미하며 fmt 태그를 사용할때 fmt를 붙여서 사용하도록 하는 것입니다. fmt는 보통 다국어 처리, 날짜/시간 형식 지정등과 관련된 기능을 제공합니다.
 
+$(".min").eq(ru).val(""); // min이라는 클래스 이름을 가진 요소에서 ru번째 인덱스의 값을 ""으로 한다.
 
 
 // 해당값을 get으로 날리고 싶다면
